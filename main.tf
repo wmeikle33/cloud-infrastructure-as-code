863d9cf23640f9420205bdaae364f5601f19fc8b
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-02-25 00:55:17 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,18 @@
+terraform {
+  required_version = ">= 1.5.0"
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-example-bucket-12345"
+}
